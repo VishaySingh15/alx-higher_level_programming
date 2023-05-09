5d8ad67d604780d746bd2f9ce73ab98fd76233ae
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-05-09 14:38:30 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(1, 100):
-    if (int(str(i)[::-1]) > i or i < 10):
-        print("{:02d}".format(i), end=", ")
     if (i == 89):
         print("{:02d}".format(i))
+    if (int(str(i)[::-1]) > i or i < 10):
+        print("{:02d}".format(i), end=", ")
