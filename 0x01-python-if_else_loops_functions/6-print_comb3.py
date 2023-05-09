764acae344082570797f8fa73d7e13b31086cd86
Vishay <vishay1998@gmail.com>
2023-05-09 04:41:24 -0700

    number 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(1, 100):
     if (int(str(i)[::-1]) > i or i < 10):
-        print("{:02d}".format(i), end = ", ")
-    if (i == 99):
-        print('')
+        print("{:02d}".format(i), end=", ")
+    if (i == 89):
+        print("{:02d}".format(i))
