ef56de75f27f24c429c8843c58292b7559d15359
Vishay <vishay1998@gmail.com>
2023-05-15 01:58:41 -0700

    updated 3 and 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    m = my_list[0]
     if len(my_list) == 0:
         return None
+    m = my_list[0]
     for i in my_list:
         if i > m:
             m = i
