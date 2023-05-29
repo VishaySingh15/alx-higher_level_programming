44ffd7d38dc137e40c358e14db20cde611c9526c
Vishay <vishay1998@gmail.com>
2023-05-29 12:28:43 -0700

    updated task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[b]}", end="")
         except Exception:
+            print()
             return count
         else:
             count += 1
