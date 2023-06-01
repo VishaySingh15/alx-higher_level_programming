602135cd6bd2f36fcf746df746c17b7830c8e390
Vishay <vishay1998@gmail.com>
2023-06-01 10:21:06 -0700

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 
+
+Addition module
+
+"""
+
+
+def add_integer(a, b=98):
+    """ 
+    Adds 2 integers and returns result
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This reasme is for alxse 0x07-python-test_driven_development and is intended to supplement my understanding of test driven development using python
